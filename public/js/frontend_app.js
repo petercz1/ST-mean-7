726738384a01bd1446f2f26c2d3c7342daaf2bb0
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 12:11:00 +0800

    You get what you pay for. 		-- Gabriel Biel

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,30 +1,33 @@
 console.log('loaded frontend app');
 
-var frontend_app = angular.module('employees',['ngRoute']);
+var frontend_app = angular.module('employees', ['ngRoute']);
 
 frontend_app.config(routes_function);
 frontend_app.controller('data_employees', do_data_employees);
-frontend_app.controller('frontend_app.controller('data_employees', do_data_employees);
-', do_data_employees);
+frontend_app.controller('single_controller', do_single_employees);
 
 function routes_function($routeProvider) {
-    console.log('doing routes stuff');
-    $routeProvider
+  console.log('doing routes stuff');
+  $routeProvider
     .when("/", {
-        templateUrl : "partials/all.html",
-        controller : "data_employees"        
+      templateUrl: "partials/all.html",
+      controller: "data_employees"
     })
     .when("/single", {
-        templateUrl : "partials/single.htm'",
-        controller : "single_controller"
+      templateUrl: "partials/single.htm'",
+      controller: "single_controller"
     });
 }
 
 function do_data_employees($scope, $http) {
-    console.log('getting employees');
-    $http.get('/api/v7/read').then(function (server_object) {
-        console.log(server_object);
-        $scope.employees = server_object.data;
-    });
+  console.log('getting employees');
+  $http.get('/api/v7/read').then(function (server_object) {
+    console.log(server_object);
+    $scope.employees = server_object.data;
+  });
 }
 
+function do_single_employees($scope, $http) {
+    console.log('getting single employee');
+    
+}
