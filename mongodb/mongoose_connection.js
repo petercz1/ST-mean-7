c1d3c4e7c9a8f5efc42e21ed5ce26be03fbf790c
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 09:43:22 +0800

    A pipe gives a wise man time to think and a fool something to stick in his mouth.

--- mongodb/mongoose_connection.js
+++ mongodb/mongoose_connection.js
@@ -1,3 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var url = 'mongodb://localhost:27500/'
+var url = 'mongodb://localhost:27500/employees';
+mongoose.connect(url, {useMongoClient: true});
+
