fcc32eb09f30f042233c87709d8da99130136218
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 11:25:35 +0800

    Bennett's Laws of Horticulture: 	(1) Houses are for people to live in. 	(2) Gardens are for plants to live in. 	(3) There is no such thing as a houseplant.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -6,5 +6,8 @@ frontend_app.controller('data_employees', do_data_employees);
 
 function do_data_employees($scope, $http) {
     console.log('getting employees');
-    $http.get('/api/v7/read').then();
+    $http.get('/api/v7/read').then(function (server_object) {
+        console.log(server_object);
+        $scope.employees = server_object.data;
+    });
 }
