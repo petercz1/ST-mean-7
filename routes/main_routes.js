2dbf3ea951726af6c745bb91d270d9a0ec5bed3a
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 10:08:38 +0800

    When among apes, one must play the ape.

--- routes/main_routes.js
+++ routes/main_routes.js
@@ -1,43 +1,58 @@
-import { clearLine } from 'readline';
-
 var router = require('express').Router();
+var EMPLOYEECLASS = require('../mongodb/mongoose_connection');
 module.exports = router;
 
 router.get('/', do_homepage);
 
 function do_homepage(req, res) {
-    console.log('doing homepage');
-    res.render('index');
+  console.log('doing homepage');
+  res.render('index');
 }
 
 // api
 
 router.get('/api/v7/read', do_read);
-router.get('/api/v7/read/:_id',do_single_read);
+router.get('/api/v7/read/:_id', do_single_read);
 router.post('/api/v7/read', do_create);
 router.put('/api/v7/read', do_update);
 router.delete('/api/v7/read', do_delete);
 
 function do_read(req, res) {
-    console.log('getting all records');
+  console.log('getting all records');
+  EMPLOYEECLASS.find({}, {
+    name: 1,
+    gender: 1
+  }).then();
 }
 
 function do_single_read(req, res) {
-    console.log('getting single record');
-    console.log(req.params);
+  console.log('getting single record');
+  console.log(req.params);
+  EMPLOYEECLASS.findById(req.params._id).then();
 }
 
 function do_create(req, res) {
-    console.log('creating employee');
-    console.log(req.body);
+  console.log('creating employee');
+  console.log(req.body);
+  var data = {
+    name: req.body.name,
+    gender: req.body.gender,
+    contact: {
+        email: req.body.email,
+        cell: req.body.cell,
+        city: req.body.city
+    }
+  }
+  var employee = new EMPLOYEECLASS(data);
+  employee.save().then();
 }
 
 function do_update(req, res) {
-    console.log('updating record');
-    console.log(req.body);
+  console.log('updating record');
+  console.log(req.body);
 }
 
 function do_delete(req, res) {
-    console.log('deleting employee');
-    console.log(req.params);
+  console.log('deleting employee');
+  console.log(req.params);
 }
