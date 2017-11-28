5768f72fd8e9bf08b0f237280f2d8ddf91a00477
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 11:24:29 +0800

    If people concentrated on the really important things in life, there'd be a shortage of fishing poles. 		-- Doug Larson

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,4 +1,10 @@
 console.log('loaded frontend app');
 
 var frontend_app = angular.module('employees',[]);
-frontend_app.controller('data_employees', do_data_employees)
+frontend_app.controller('data_employees', do_data_employees);
+
+
+function do_data_employees($scope, $http) {
+    console.log('getting employees');
+    $http.get('/api/v7/read').then();
+}
