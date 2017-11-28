2f187eb2005ec1196b4ced933fcbb8c223a3d724
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 09:44:36 +0800

    The trouble with being poor is that it takes up all your time.

--- mongodb/mongoose_connection.js
+++ mongodb/mongoose_connection.js
@@ -3,3 +3,8 @@ mongoose.Promise = global.Promise;
 var url = 'mongodb://localhost:27500/employees';
 mongoose.connect(url, {useMongoClient: true});
 
+var contact_structure = {
+    email: String,
+    cell: String,
+    city: String
+}
