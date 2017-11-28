04a636b137e3096d5bbbf0e59f652c8f68b0fb06
petercz1 <email.peter.carroll@gmail.com>
2017-11-28 11:26:04 +0800

    Have you seen the latest Japanese camera?  Apparently it is so fast it can photograph an American with his mouth shut!

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -3,7 +3,6 @@ console.log('loaded frontend app');
 var frontend_app = angular.module('employees',[]);
 frontend_app.controller('data_employees', do_data_employees);
 
-
 function do_data_employees($scope, $http) {
     console.log('getting employees');
     $http.get('/api/v7/read').then(function (server_object) {
